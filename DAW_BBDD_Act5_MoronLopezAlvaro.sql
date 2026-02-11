4fc89741152e3532b55090ca27feb42025cd7a4e
moronlopezalvaro <moronlopezalvaro@gmail.com>
2026-02-11 10:16:13 +0100

    creo la base de datos

---
+++ DAW_BBDD_Act5_MoronLopezAlvaro.sql
@@ -0,0 +1,5 @@
+-- 1.- Crea una base de datos de nombre “hym_bd”.
+DROP DATABASE IF EXISTS hym_bd; -- Depuramos la base de datos
+CREATE DATABASE IF NOT EXISTS hym_bd; -- Creamos la base de datos
+
+USE hym_bd; -- Usamos la base de datos que acabamos de crear
